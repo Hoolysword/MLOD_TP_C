8564dca0bf8e0fd212394b4695cf95aae4569f9f
Robin <bukielskirobin@gmailcom>
2022-10-12 10:29:44 +0200

    exo manip date

---
+++ TP2_Pointeur/manip_date.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum mois {
+    janvier,
+    fevrier,
+    mars,
+    avril,
+    mai,
+    juin,
+    juillet,
+    aout,
+    septembre,
+    octobre,
+    novembre,
+    decembre,
+};
+typedef struct {
+    int jour;
+    enum mois mois;
+    int annee;
+} Date;
+
+void initialiseDate(Date *date){
+    scanf("%i",(int *)&date->jour);
+    scanf("%i",(int *)&date->mois);
+    scanf("%i",(int *)&date->annee);
+}
+void afficheDate(Date *date){
+    printf("%i/%i/%i",date->jour,date->mois,date->annee);
+}
+Date creerDateParCopie(){
+    Date d;
+    initialiseDate(&d);
+    return d;
+}
+int main(void) {
+//version 1
+/*Date d;
+initialiseDate(&d); //Pourquoi a t-on ajouté un &?
+afficheDate(&d);*/
+//version 2
+Date d;
+d = creerDateParCopie();
+afficheDate(&d);
+
+} 
